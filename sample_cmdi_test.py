c20cfc3231bc46e5d12c56eb8e181491d7be3ed3
Siponek <szinkowicz@gmail.com>
2023-05-10 01:59:39 +0200

    Fixed calculation of the tests

--- sample_cmdi_test.py
+++ sample_cmdi_test.py
@@ -6,10 +6,11 @@ import pathlib
 import yaml
 import pprint
 import asyncio
+import random
 from typing import Self
 from tqdm import tqdm
+from time import sleep
 from collections import ChainMap
-import random
 import colorama
 from colorama import Fore, Back, Style
 
@@ -17,88 +18,99 @@ colorama.init()
 pp = pprint.PrettyPrinter(indent=4, width=43, compact=True)
 
 WHOAMI_ORACLE: str
-QUERY_PARAMS: list
+QUERY_PARAMS: dict
 COMMAND_INJECTION_CMD: dict
 ARGUMENT_INJECTION_CMD: dict
 BLIND_COMMAND_INJECTION_CMD: dict
 XSS_CMD: dict
-list_of_routes_in_folder: list
 MARKS: dict = {
     "checkmark": "\u2705",
     "failmark": "\u274c",
 }
+HTTP_BASE: str = "http://localhost:9000/"
 
 
-with open("config.yaml", "r") as ymlfile:
-    attack_config: dict = yaml.load(ymlfile, Loader=yaml.CLoader)
-    # Get the variable values
-    user_name: str = attack_config["variables"]["user_name"]
-    folder_to_watch: str = attack_config["variables"]["folder_to_watch"]
-    query_params: list = attack_config["variables"]["query_params"]
-    command: str = attack_config["variables"]["command"]
-    replaced_config: dict = {}
-    print(attack_config)
-    for attack_type in attack_config.keys():
-        print(Fore.CYAN + attack_type + Fore.RESET)
-        if attack_type == "variables":
-            continue
-        replaced_config[attack_type] = {}
-        for attack_key, attack_value in attack_config[attack_type].items():
-            replaced_config[attack_type][attack_key] = {}
+def process_config_file(config_file_path: str) -> dict:
+    """Function for processing the config file
 
-            for key in attack_value:
-                replaced_config[attack_type][attack_key][key] = (
-                    attack_config[attack_type][attack_key][key]
-                    .replace(r"${ variables.user_name }", user_name)
-                    .replace(
-                        r"${ variables.folder_to_watch }", folder_to_watch
+    Args:
+        config_file_path (str): Path to the config file
+
+    Returns:
+        dict: Dictionary with the config file values replaced
+    """
+    with open(config_file_path, "r") as ymlfile:
+        attack_config: dict = yaml.load(ymlfile, Loader=yaml.CLoader)
+        # Get the variable values
+        user_name: str = attack_config["variables"]["user_name"]
+        folder_to_watch: str = attack_config["variables"]["folder_to_watch"]
+        # query_params: list = attack_config["variables"]["query_params"]
+        command: str = attack_config["variables"]["command"]
+        replaced_config: dict = {}
+        for attack_type in attack_config.keys():
+            if attack_type == "variables":
+                continue
+            print(Fore.CYAN + f"Config processing {attack_type}" + Fore.RESET)
+            replaced_config[attack_type] = {}
+            for attack_key, attack_value in attack_config[attack_type].items():
+                replaced_config[attack_type][attack_key] = {}
+                for key in attack_value:
+                    replaced_config[attack_type][attack_key][key] = (
+                        attack_config[attack_type][attack_key][key]
+                        .replace(r"${ variables.user_name }", user_name)
+                        .replace(
+                            r"${ variables.folder_to_watch }", folder_to_watch
+                        )
+                        .replace(r"${ variables.command }", command)
                     )
-                    .replace(r"${ variables.command }", command)
-                )
-            print(attack_key)
-            print(attack_value)
-    print(Fore.CYAN, attack_config, Fore.RESET)
-    WHOAMI_ORACLE = attack_config["variables"]["user_name"]
-    COMMAND_INJECTION_CMD = attack_config["command_injection"]
-    ARGUMENT_INJECTION_CMD = attack_config["argument_injection"]
-    BLIND_COMMAND_INJECTION_CMD = attack_config["blind_command_injection"]
-    XSS_CMD = attack_config["xss"]
-    # pp.pprint(ARGUMENT_INJECTION_CMD)
+        replaced_config["variables"] = attack_config["variables"]
+        return replaced_config
 
 
-class report:
+class TestingClass:
     """Class for generating a report of the tests performed"""
 
     def __init__(self) -> None:
-        self.test_report = {}
-        self.all_tests_length = (
-            len(COMMAND_INJECTION_CMD)
-            + len(ARGUMENT_INJECTION_CMD)
-            + len(BLIND_COMMAND_INJECTION_CMD)
-            + len(XSS_CMD)
+        self.test_report: dict = {}
+        self.secondary_position: int = 1
+        self.test_route: list = []
+
+    @property
+    def all_tests_length(self) -> int:
+        return sum(
+            [
+                len(dct.values())
+                for dct in [
+                    COMMAND_INJECTION_CMD,
+                    ARGUMENT_INJECTION_CMD,
+                    BLIND_COMMAND_INJECTION_CMD,
+                    XSS_CMD,
+                ]
+            ]
         )
-        self.secondary_position = 1
 
     def append_to_report(
         self, route: str, test_name: str, test_result: bool
     ) -> Self:
         if route not in self.test_report:
+            tqdm.write(f"Appending current route path to report for: {route}")
             self.test_report[route] = {}
         self.test_report[route][test_name] = test_result
         return self
 
     def calcualte_passed_tests(self, route: str) -> Self:
-        for route in self.test_report:
-            self.test_report[route]["passed_tests"] = 0
-            for test in self.test_report[route]:
-                if self.test_report[route][test]:
-                    self.test_report[route]["passed_tests"] += 1
+        self.test_report[route]["passed_tests"] = 0
+        for test in self.test_report[route]:
+            # tqdm.write(f"test {test} {self.test_report[test]}")
+            if self.test_report[route][test] and test != "passed_tests":
+                self.test_report[route]["passed_tests"] += 1
         return self
 
     def calculate_total_tests(self) -> Self:
         for route in self.test_report:
-            self.test_report[route]["total_tests"] = len(
-                self.test_report[route]
+            tqdm.write(f"route {route} {self.test_report[route]}")
+            self.test_report[route]["total_tests"] = (
+                len(self.test_report[route]) - 1
             )
         return self
 
@@ -109,96 +121,143 @@ class report:
                 / self.test_report[route]["total_tests"]
                 * 100
             )
+        # self._reset()
         return self
 
-    def test_suite(self, array_of_dicts: list, route) -> Self:
+    def test_suite(
+        self,
+        array_of_dicts: list,
+        file_route: str,
+        route_type: list,
+        http_adress: str = "http://localhost:9000/",
+    ) -> Self:
+        self.test_route = route_type
+        amount_of_tests: int = 0
         for test_dict in array_of_dicts:
+            if route_type[0] not in test_dict:
+                continue
+            amount_of_tests = sum([len(test_dict[x]) for x in test_dict])
             with tqdm(
-                total=len(test_dict),
+                total=amount_of_tests,
                 position=self.secondary_position,
                 colour="CYAN",
                 leave=False,
             ) as pbar2:
-                for test_name, test_value in test_dict.items():
-                    pbar2.set_description(
-                        f"Testing: \033[1m{test_name}\033[0m"
-                    )
-                    complete_route: str = f"http://localhost:9000/{route}"
-                    test_result = test_step(
-                        test_name=test_name,
-                        test_value=test_value,
-                        url_for_request=complete_route,
-                    )
-                    self.append_to_report(
-                        route=route,
-                        test_name=test_name,
-                        test_result=test_result,
-                    )
-                    pbar2.update(1)
+                for test_type, test_value in test_dict.items():
+                    if test_type != route_type[0]:
+                        pbar2.update(1)
+                        continue
+                    for test in test_value:
+                        pbar2.set_description(
+                            f"Testing:{Fore.GREEN + Style.BRIGHT}{test_type} {test}{Fore.RESET + Style.RESET_ALL}"
+                        )
+                        test_result = self.test_step(
+                            test_value=test_value[test],
+                            url_for_request=http_adress + file_route,
+                        )
+                        self.append_to_report(
+                            route=file_route,
+                            test_name=test_type + " " + test,
+                            test_result=test_result,
+                        )
+                        pbar2.update(1)
             pbar2.close()
             # self.secondary_position += 1
         return self
 
+    def test_step(self, test_value: str, url_for_request: str) -> bool:
+        """Function for testing a single test case"""
+        # return random.choice([True, False])
+        cookies: dict = {"a": "1"}
+        headers: dict = {}
+        params: dict = {self.test_route[1]: test_value}
+        response: requests.Response = requests.Response()
+        try:
+            response = requests.get(
+                url=url_for_request,
+                params=params,
+                cookies=cookies,
+                headers=headers,
+            )
+        except requests.exceptions.ConnectionError:
+            tqdm.write("Connection error. Check if the server is running.")
+            exit()
+        return not WHOAMI_ORACLE in response.text
 
-def test_step(test_name: str, test_value: str, url_for_request: str) -> bool:
-    """Function for testing a single test case"""
-    return random.choice([True, False])
-    cookies: dict = {"a": "1"}
-    headers: dict = {}
-    params: dict = {x: test_value for x in QUERY_PARAMS}
-    response: requests.Response = requests.Response()
-    try:
-        response = requests.get(
-            url=url_for_request,
-            params=params,
-            cookies=cookies,
-            headers=headers,
+    def _reset(self) -> Self:
+        "I have messed up somewhere and the tests are calculating incorrectly. This is a quick fix."
+        self.test_report = {}
+        self.secondary_position = 1
+        return self
+
+
+def define_testing_type(testing_route: str) -> list:
+    if testing_route.find("echo") != -1:
+        return ["echo", QUERY_PARAMS["echo"]]
+    elif testing_route.find("ping") != -1:
+        return ["ping", QUERY_PARAMS["ping"]]
+    elif testing_route.find("find") != -1:
+        return ["find", QUERY_PARAMS["find"]]
+    elif testing_route.find("exec") != -1:
+        return ["exec", QUERY_PARAMS["exec"]]
+    raise ValueError(
+        f"No testing type found for given route {Fore.RED +  testing_route + Fore.RESET}. Make sure that the page has either exec, echo, find, or ping in the name."
+    )
+
+
+async def update_progress_bar(pbar_test, passed_tests, total_tests):
+    while True:
+        pbar_test.set_description(
+            f"{MARKS['checkmark']}Passed {passed_tests} out of {total_tests-1}"
         )
-    except requests.exceptions.ConnectionError:
-        print("Connection error. Check if the server is running.")
-        exit()
-    return not WHOAMI_ORACLE in response.text
+        pbar_test.update()
+        await asyncio.sleep(0.1)
 
 
-def main(*args, **kwargs):
+async def main(*args, **kwargs):
     """Main function for running the tests"""
-    current_route_test_report = report()
+    TestClass: TestingClass = TestingClass()
     array_of_tests: list = [
         COMMAND_INJECTION_CMD,
         ARGUMENT_INJECTION_CMD,
         BLIND_COMMAND_INJECTION_CMD,
         XSS_CMD,
     ]
-    list_of_routes_in_folder = [
+    list_of_routes_in_folder: list = [
         str(path)
         for path in pathlib.Path().iterdir()
         if path.is_file()
         and path.suffix == ".php"
         and path.name not in ["index.php", "router.php"]
     ]
-    pp.pprint(list_of_routes_in_folder)
-
-    tmp_dict = dict(ChainMap(*array_of_tests))
     # TODO make this async for every route
     with tqdm(total=len(list_of_routes_in_folder), position=0) as pbar1:
         for testing_route in list_of_routes_in_folder:
+            route_type: list = []
+            try:
+                route_type = define_testing_type(testing_route)
+            except ValueError as errno:
+                tqdm.write(f"Error: {errno}")
+                continue
             pbar1.set_description(
                 f"Testing route: \033[1m{testing_route}\033[0m"
             )
             pbar1.update(1)
-            complete_route: str = f"http://localhost:9000/{testing_route}"
-            current_route_test_report.test_suite(
-                array_of_dicts=array_of_tests, route=complete_route
-            ).calcualte_passed_tests(
-                route=testing_route
-            ).calculate_total_tests().calculate_passed_tests_percentage()
+            # Here the http_adress could be the base attr for the class since it is global and read from config.yaml
+            TestClass.test_suite(
+                array_of_dicts=array_of_tests,
+                http_adress=HTTP_BASE,
+                file_route=testing_route,
+                route_type=route_type,
+            ).calcualte_passed_tests(route=testing_route)
+        TestClass.calculate_total_tests().calculate_passed_tests_percentage()
         pbar1.close()
-    for route, values in current_route_test_report.test_report.items():
+    for route, values in TestClass.test_report.items():
         print(
             f"\033[1m{route}\033[0m",
-            # f"{MARKS['checkmark']} Passed {values['passed_tests']} out of {values['total_tests']}",
         )
         pbar_test = tqdm(
+            # passed_test is always 1 less than total_tests beacuse of the passed_tests key
             total=values["total_tests"],
             position=1,
             colour="GREEN",
@@ -210,16 +269,27 @@ def main(*args, **kwargs):
         pbar_test.close()
         print(f"\n{MARKS['failmark']} Falied tests:")
         pp.pprint([x for x in values if not values[x]])
-    result: float = 0
+    result: int = 0
+    total_tests_ran: int = 0
     print(f"Overall suite result:")
-    for route, values in current_route_test_report.test_report.items():
+    for route, values in TestClass.test_report.items():
+        print(
+            f"\033[1m{route}\033[0m has {values['passed_tests']} tests passed out of {values['total_tests']}"
+        )
         result += values["passed_tests"]
-    print(
-        f"{MARKS['checkmark']} Passed {result} out of {current_route_test_report.all_tests_length*len(list_of_routes_in_folder)}"
-    )
+        total_tests_ran += values["total_tests"]
+    print(f"{MARKS['checkmark']} Passed {result} out of {total_tests_ran}")
 
 
 if __name__ == "__main__":
     import sys
 
-    main(sys.argv[1:])
+    yaml_config = process_config_file("config.yaml")
+    WHOAMI_ORACLE = yaml_config["variables"]["user_name"]
+    COMMAND_INJECTION_CMD = yaml_config["command_injection"]
+    ARGUMENT_INJECTION_CMD = yaml_config["argument_injection"]
+    BLIND_COMMAND_INJECTION_CMD = yaml_config["blind_command_injection"]
+    XSS_CMD = yaml_config["xss"]
+    QUERY_PARAMS = yaml_config["variables"]["query_params"]
+    HTTP_BASE = yaml_config["variables"]["base_url"]
+    asyncio.run(main(sys.argv[1:]))
